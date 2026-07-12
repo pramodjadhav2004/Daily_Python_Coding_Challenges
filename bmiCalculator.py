31fac7e5409d84f4d67effd215da7442d55e43ff
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-12 23:40:49 +0530

    feat: adding BMI calculation function

---
+++ bmiCalculator.py
@@ -0,0 +1,9 @@
+#23-06-26
+#Given a weight in pounds and a height in inches, return the BMI (Body Mass Index) rounded to one decimal place.
+#To get BMI: divide the weight by the height squared, then multiply the result by 703.
+def calculate_bmi(weight, height):
+    bmi=(weight/height**2)*703
+    return round(bmi,1)
+
+bmi=calculate_bmi(180, 70)
+print(bmi)
