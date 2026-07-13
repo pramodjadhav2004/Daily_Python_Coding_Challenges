9ce035f7588e16ef54a7710941f327a1c93bce2d
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-13 23:56:33 +0530

    feat: add higher/lower validation logic for number guessing

---
+++ secretNumber.py
@@ -0,0 +1,17 @@
+#25-05-26
+"""
+Given a secret number and a guess, determine if the guess is correct.
+Return:
+"higher" if the secret number is higher than the guess.
+"lower" if the secret number is lower than the guess.
+"you got it!" if the guess is correct.
+"""
+def guess_number(secret, guess):
+    if guess==secret:
+        return "you got it!"
+    elif guess>secret:
+        return "lower"
+    elif guess<secret:
+        return "higher"
+msg=guess_number(85, 99)
+print(msg)
