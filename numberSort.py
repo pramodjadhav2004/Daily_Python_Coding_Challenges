b1f6d4c434378eece68ba48df32bd7d3d483e9bc
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-13 23:45:29 +0530

    updated the code to take number string parameter as user input

--- numberSort.py
+++ numberSort.py
@@ -1,11 +1,11 @@
 #15-06-26
 #Given a string of numbers separated by commas, return an array of the numbers sorted from smallest to largest.
 def sort_numbers(s):
-    num_list=[]
+    num_list_1=[]
     nlist=s.split(",")
     for i in nlist:
-        num_list+=[int(i)]
-    num_list.sort()
+        num_list_1+=[int(i)]
+    num_list_1.sort()
     return num_list
 
 string=input("Enter a string of numbers separated by commas: ")
