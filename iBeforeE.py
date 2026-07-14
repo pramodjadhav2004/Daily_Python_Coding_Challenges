6b7856ef05e36da069819c71b586a640add11ced
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-14 23:56:34 +0530

    feat: implemented I before E spelling correction function

---
+++ iBeforeE.py
@@ -0,0 +1,28 @@
+#21-06-26
+"""
+Given a word or sentence, return a corrected version where every word
+follows the "I before E except after C" rule.
+
+- If a word contains "ei" not preceded by "c", replace it with "ie".
+- If a word contains "ie" preceded by "c", replace it with "ei".
+- All other words are left unchanged.
+"""
+def i_before_e(sentence):
+    newSent=""
+    flag=0
+    for i in range(len(sentence)-1):
+        if flag==1:
+            flag=0
+            continue
+        if sentence[i]=='e' and sentence[i+1]=='i' and sentence[i-1]!='c':
+            newSent+="ie"
+            flag=1
+        elif sentence[i]=='i' and sentence[i+1]=='e' and sentence[i-1]=='c':
+            newSent+="ei"
+            flag=1
+        else:
+            newSent+=sentence[i]
+            flag=0
+    return newSent+sentence[-1]
+newSent=i_before_e("we recieved a breif")
+print(newSent)
