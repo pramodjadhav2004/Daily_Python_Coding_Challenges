46431c70664c3940fde5fa943fd2a01024843a35
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-21 23:53:08 +0530

    implemented  string decompression by positional word replacement

---
+++ wordDecompressor.py
@@ -0,0 +1,19 @@
+#25-04-26
+"""
+Given a compressed string, return the decompressed version using the following rules:
+
+- The given string is made up of words and numbers separated by spaces.
+- Leave the words unchanged.
+- Replace numbers with the word at that position, where the first word is at position 1.
+"""
+def decompress(s):
+    list_s=s.split()
+    new_s=[]
+    for i in list_s:
+        if i.isdigit():
+            new_s.append(list_s[int(i)-1])
+        else:
+            new_s.append(i)
+    return " ".join(new_s)
+new_s=decompress("practice makes perfect and 3 1 2 3")
+print(new_s)
