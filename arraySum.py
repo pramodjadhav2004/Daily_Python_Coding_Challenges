2fb82284732226907c9d7d592fec00da18cafceb
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-23 23:52:54 +0530

    implemented function to return sum of numbers in list

---
+++ arraySum.py
@@ -0,0 +1,7 @@
+#09-03-26
+#Given an array of numbers, return the sum of all the numbers.
+def sum_array(numbers):
+    summ=sum(numbers)
+    return summ
+summ=sum_array([203, 145, -129, 6293, 523, -919, 845, 2434])
+print(summ)
