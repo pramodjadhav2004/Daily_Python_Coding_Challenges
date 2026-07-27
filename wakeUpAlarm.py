88acc7f753879f83f4a74fb7a7048e20b06ae3e7
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-27 23:26:31 +0530

    updated the code to take alarm time and wake time as user input

--- wakeUpAlarm.py
+++ wakeUpAlarm.py
@@ -26,5 +26,7 @@ def alarm_check(alarm_time, wake_time):
     else:
         return "late"
 
-ans=alarm_check("08:10", "08:15")
+alarm_time=input("Enter the alarm time in HH:MM format: ")
+wake_time=input("Enter the wake up time in HH:MM format: ")
+ans=alarm_check(alarm_time, wake_time)
 print(ans)
