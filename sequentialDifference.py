07a8b13cf3324f58d70f35fc0de0bdde12cbe547
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-28 01:15:18 +0530

    updated the code to take array of numbers as user input

--- sequentialDifference.py
+++ sequentialDifference.py
@@ -4,5 +4,7 @@ def find_differences(arr):
         diff.append(arr[i+1]-arr[i])
     diff.append(0)
     return diff
-diff=find_differences([10, 15, 19, 22, 24, 25])
+arr=input("Enter an array of numbers separated by commas: ")
+arr=[int(x) for x in arr.split(",")]
+diff=find_differences(arr)
 print(diff)
