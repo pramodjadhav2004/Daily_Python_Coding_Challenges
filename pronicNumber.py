8a50226441a23418a6bc4c53fc85e13fa8792072
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-27 23:25:02 +0530

    code to determine whether a given number is pronic

---
+++ pronicNumber.py
@@ -0,0 +1,10 @@
+#27-07-26
+#Given a number, determine whether it is a pronic number.
+#A pronic number is the product of two consecutive integers. For example, 6 is pronic because 2 * 3 = 6.
+def is_pronic(n):
+    for i in range(n+1):
+        if i*(i+1)==n:
+            return True
+    return False
+ans=is_pronic(12)
+print(ans)
