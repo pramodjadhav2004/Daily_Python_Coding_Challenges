52b95e610c5a71c8256fa5dbba6c856901724a1d
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-29 21:46:39 +0530

    code to print the mirror version of a string

---
+++ stringMirror.py
@@ -0,0 +1,6 @@
+#03-02-26
+#Given a string, return a new string that consists of the given string with a reversed copy of itself appended to the end of it.
+def mirror(s):
+    s+=s[::-1]
+    return s
+print(mirror("freeCodeCamp"))
