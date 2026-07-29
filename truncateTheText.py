824c5a3a8ec494d8851f25cc3617c1d2c439c404
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-29 22:34:06 +0530

    code to truncate a particular text to 20 characters only

---
+++ truncateTheText.py
@@ -0,0 +1,9 @@
+#04-02-26
+#Given a string, return it as-is if it's 20 characters or shorter. If it's longer than 20 characters, truncate it to the first 17 characters and append "..." to the end of it (so it's 20 characters total) and return the result.
+def truncate_text(text):
+    lenn=len(text)
+    if lenn<=20:
+        return text
+    else:
+        return text[:17]+"..."
+print(truncate_text("This string should get truncated."))
