9d3057fc72afc285fafa63b7333f0dfabe836e49
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-31 23:37:41 +0530

    implemented function to insert hyphens between letters and numbers

---
+++ Letters-Numbers.py
@@ -0,0 +1,16 @@
+#29-01-26
+#Given a string containing only letters and numbers, return a new string where a hyphen (-) is inserted every time the string switches from a letter to a number, or a number to a letter.
+def separate_letters_and_numbers(s):
+    newS=""
+    flag=0
+    for i in range(len(s)-1):
+        newS+=s[i]
+        if flag==1 and s[i+1].isalpha():
+            flag=0
+            newS+="-"
+        if s[i+1].isdigit() and s[i].isalpha():
+            newS+="-"
+            flag=1
+    return newS+s[-1]
+
+print(separate_letters_and_numbers("a1b2c3d4"))
