ea0431175d5fe30b56cae33fc3e1ea36c30ade6d
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-07-31 23:27:38 +0530

    implemented biathlon penalty distance calculator

---
+++ biathlon.py
@@ -0,0 +1,15 @@
+#08-02-26
+"""
+Given an array of integers, where each value represents the number of
+targets hit in a single round of a biathlon, return the total penalty distance
+the athlete must ski.
+
+- Each round consists of 5 targets.
+- Each missed target results in a 150 meter penalty loop.
+"""
+def calculate_penalty_distance(rounds):
+    penalty=0
+    for i in rounds:
+        penalty+=(5-i)*150
+    return penalty
+print(calculate_penalty_distance([4, 3, 0, 3]))
