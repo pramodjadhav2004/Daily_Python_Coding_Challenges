ee1ced5e43a0c08b83c1d1c5ba89dd8fb0d06164
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-08-01 23:53:42 +0530

    implemented function to check whether two strings are anagrams or not

---
+++ anagramChecker.py
@@ -0,0 +1,13 @@
+#16-08-26
+#Given two strings, determine if they are anagrams of each other (contain the same characters in any order).
+#Ignore casing and white space.
+
+def are_anagrams(str1, str2):
+    set1=set(str1.lower())
+    set2=set(str2.lower())
+    str1=list(set1)
+    str2=list(set2)
+    str1.sort()
+    str2.sort()
+    return str1==str2
+print(are_anagrams("A gentleman", "Elegant man"))
