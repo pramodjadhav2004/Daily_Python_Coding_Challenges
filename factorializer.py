3f35e520dfd7b435bfba425bf5b3fa44bdc76202
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-08-02 00:00:57 +0530

    implemented recursive function to print factorial of any number

---
+++ factorializer.py
@@ -0,0 +1,8 @@
+#18-08-26
+#Given an integer from zero to 20, return the factorial of that number. The factorial of a number is the product of all the numbers between 1 and the given number.
+#The factorial of zero is 1.
+def factorial(n):
+    if n==0:
+        return 1
+    return n*factorial(n-1)
+print(factorial(5))
