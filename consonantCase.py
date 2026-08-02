796eea951fabfe825898b09028c6de6370629ab2
pramodjadhav2004 <pramodjadhav0513@gmail.com>
2026-08-03 00:03:52 +0530

    implement consonant case string conversion

---
+++ consonantCase.py
@@ -0,0 +1,25 @@
+#20-01-26
+"""
+    Given a string representing a variable name, convert it to consonant case using the 
+    following rules:
+
+    - All consonants should be converted to uppercase.
+    - All vowels (a, e, i, o, u in any case) should be converted to lowercase.
+    - All hyphens (-) should be converted to underscores (_).
+    """
+vowel="aeiou"
+def to_consonant_case(s):
+    vowelcase=""
+    for i in s:
+        if i.isalpha():
+            if i.lower() in vowel:
+                vowelcase+=i.lower()
+            else:
+                vowelcase+=i.upper()
+        else:
+            if i=="-":
+                vowelcase+="_"
+            else:
+                vowelcase+=i
+    return vowelcase
+print(to_consonant_case("_hElLO-WOrlD-"))
